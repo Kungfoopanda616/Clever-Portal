97a2a541cf64a469bac04efc20ab28bfdcf94795
Kungfoopanda616 <emerycarterbrown123@gmail.com>
2026-03-30 20:55:12 -0500

    uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,7 +1,6 @@
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 
-// This uses the core engine from the bundle you pasted earlier
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
