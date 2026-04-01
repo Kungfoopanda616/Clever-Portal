bbd447f8c2b25eb0969042579b97dd2078d32c30
Kungfoopanda616 <emerycarterbrown123@gmail.com>
2026-04-01 17:28:38 -0500

    service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+// Basic service worker setup
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+  // For now, just fetch from network
+  event.respondWith(fetch(event.request));
+});
